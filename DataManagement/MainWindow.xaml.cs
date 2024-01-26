b839602b0ac6d82255cd685763e7955c8b273a08
amuolo <and.muolo@gmail.com>
2024-01-26 16:17:26 +0100

    minor cleanup

--- DataManagement/MainWindow.xaml.cs
+++ DataManagement/MainWindow.xaml.cs
@@ -11,7 +11,7 @@ namespace DataManagement;
 
 public interface IContract : IDataContract {}
 
-public record MainWindowState(string Path, Model Data, DataWindow DataWindow);
+public record MainWindowState(string Path, DataWindow DataWindow);
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
@@ -30,13 +30,14 @@ public partial class MainWindow : Window
         Application.Current.DispatcherUnhandledException += new DispatcherUnhandledExceptionEventHandler((s, e) => Logger($"Dispatcher Unhandled Exception"));
         TaskScheduler.UnobservedTaskException += new EventHandler<UnobservedTaskExceptionEventArgs>((s, e) => Logger($"Unobserved Task Exception Event"));
 
+        // TODO: improve this file picker
         var dirInfo = new DirectoryInfo(".");
         var files = dirInfo.GetFiles("*.*", SearchOption.AllDirectories);
         InputFilePicker.ItemsSource = files;
 
         Office = Office<IContract>.Create().AddAgent<Model, DataHub, IDataContract>().Run();
 
-        State = new(dirInfo.FullName, new Model(), new DataWindow());
+        State = new(dirInfo.FullName, new DataWindow());
     }
 
     private void OpenDataWindowClick(object sender, RoutedEventArgs e) => State.DataWindow.Show();
@@ -70,6 +71,7 @@ public partial class MainWindow : Window
 
     private void ExportClick(object sender, RoutedEventArgs e)
     {
+        /* TODO: remove this code once the actor model fully support all these functionalities
         var name = ExportFileName?.Text;
         var fileName = State.Path + "\\" + name?? "";
         var progressBar = new ProgressBar();
@@ -77,6 +79,7 @@ public partial class MainWindow : Window
         JobFactory.New().WithOptions(o => o.WithLogs(Logger).WithProgress(progressBar.Enable, progressBar.Update, progressBar.Disable))
                         .WithStep($"Export to file {name}", () => DataOperator.Export(State.Data.GetPrintable(), fileName))
                         .Start();
+        */
     }
 
     private void ConnectClick(object sender, RoutedEventArgs e)
