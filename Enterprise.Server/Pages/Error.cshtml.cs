99fd10ed81da151ab72f88c867a34e41ef7cb17b
amuolo <and.muolo@gmail.com>
2024-04-16 09:42:25 +0200

    rename enterprise server

--- Enterprise.Server/Pages/Error.cshtml.cs
+++ Enterprise.Server/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace Service.Dispatcher.Pages
+namespace Enterprise.Server.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
