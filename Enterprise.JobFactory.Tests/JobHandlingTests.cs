51cf627434147e2dd1a2e6177a634fc90e60a8f9
amuolo <and.muolo@gmail.com>
2024-04-21 14:12:29 +0200

    job factory more tests

--- Enterprise.JobFactory.Tests/JobHandlingTests.cs
+++ Enterprise.JobFactory.Tests/JobHandlingTests.cs
@@ -13,7 +13,7 @@ public class TestsJobMachine
     public async Task SimpleStateManipulations()
     {
         var r = await JobFactory.New(-1)
-            .WithOptions(o => o.WithLogs(Logger))//.WithProgress(progressBar.Enable, progressBar.Update, progressBar.Disable))
+            .WithOptions(o => o.WithLogs(Logger))
             .WithStep($"s1", n0 => 1)
             .WithStep($"s2", n1 => n1 + 10)
             .WithStep($"s3", n2 => n2 + 20)
@@ -49,9 +49,30 @@ public class TestsJobMachine
             .WithStep($"s2", n1 => $"{n1+2}")
             .WithStep($"s3", n2 => int.Parse(n2) + 6.9)
             .WithStep($"s4", async n3 => { await Task.Delay(10); return n3; })
+            .WithStep($"s5", async n4 => { await Task.Delay(10); return $"{n4}"; })
+            .WithStep($"s6", n5 => "." + n5)
             .Start();
 
-        Assert.AreEqual(9.9, r.State);
-        Assert.AreEqual(4, Logs.Count);
+        Assert.AreEqual(".9.9", r.State);
+        Assert.AreEqual(6, Logs.Count);
+    }
+
+    [TestMethod]
+    public async Task ProgressHandling()
+    {
+        int nTot = -1, nSteps = 0, nClose = -1;
+
+        var r = await JobFactory.New()
+            .WithOptions(o => o.WithLogs(Logger).WithProgress(n => nTot = n, () => nSteps++, () => nClose = 1))
+            .WithStep("s1", _ => 1)
+            .WithStep("s2", _ => 2)
+            .WithStep("s3", _ => 3)
+            .Start();
+
+        Assert.AreEqual(3, r.State);
+        Assert.AreEqual(3, Logs.Count);
+        Assert.AreEqual(3, nTot);
+        Assert.AreEqual(3, nSteps);
+        Assert.AreEqual(1, nClose);
     }
 }
