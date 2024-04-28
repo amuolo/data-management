0b93463379c092b7aa4a725cbbd099cff36f281f
amuolo <and.muolo@gmail.com>
2024-04-28 11:40:40 +0200

    increaes job thread safety tests

--- Enterprise.JobFactory.Tests/ThreadSafetyTests.cs
+++ Enterprise.JobFactory.Tests/ThreadSafetyTests.cs
@@ -13,18 +13,56 @@ public class ThreadSafetyTests
     public async Task BasicSimultaneousIncrements()
     {
         var job = Job.JobFactory.New()
-            .WithStep("s1", async () => { await Task.Delay(100); return new MyTypeX() { X = 0, Y = 0 }; });
+            .WithStep("s1", () => new MyTypeX() { X = 0, Y = 0 });
 
         job.Start();
 
         Enumerable.Range(0, 10).ToList().ForEach(i =>
         {
-            Task.Run(() => job.WithStep($"x{i}", async s => { await Task.Delay(10 + i); s.X++; s.Y--; }).Start());
+            Task.Run(() => job.WithStep($"x{i}", s => { s.X++; s.Y--; }).Start());
         });
 
         await job.Start();
 
         Assert.AreEqual(+10, job.State?.X);
         Assert.AreEqual(-10, job.State?.Y);
+    }
+
+    [TestMethod]
+    public async Task BasicSimultaneousIncrementsAsync()
+    {
+        var job = Job.JobFactory.New()
+            .WithStep("s1", async () => { await Task.Delay(100); return new MyTypeX() { X = 0, Y = 0 }; });
+
+        job.Start();
+
+        Enumerable.Range(0, 10).ToList().ForEach(i =>
+        {
+            Task.Run(() => job.WithStep($"x{i}", async s => { await Task.Delay(10); s.X++; s.Y--; }).Start());
+        });
+
+        await job.Start();
+
+        Assert.AreEqual(+10, job.State?.X);
+        Assert.AreEqual(-10, job.State?.Y);
+    }
+
+    [TestMethod]
+    public async Task BasicSimultaneousIncrementsAsyncWithPrimitiveType()
+    {
+        var job = Job.JobFactory.New()
+            .WithStep("s1", async () => { await Task.Delay(100); return 0; });
+
+        job.Start();
+
+        Enumerable.Range(0, 10).ToList().AsParallel().ForAll(i =>
+        {
+            job.WithStep($"x{i}", async n => { await Task.Delay(10); return n++; }).Start();
+        });
+
+        await job.Start();
+
+        // TODO: check if intended behavior, i.e. if use case exists for implementing state update for value types
+        Assert.AreEqual(0, job.State);
     }
 }
