d7b64390bebfe753f7c54fe6ba988767953dc854
amuolo <and.muolo@gmail.com>
2024-05-02 11:41:13 +0200

    agency better test coverage

--- Enterprise.Agency.Tests/AgentsTests.cs
+++ Enterprise.Agency.Tests/AgentsTests.cs
@@ -146,6 +146,39 @@ public class AgentsTests
         Assert.AreEqual("Data has been processed", display);
         Assert.IsTrue(Storage.Any(x => x.Message.Contains(nameof(IHubContract.CreateRequest)) && x.Sender == agentName));
     }
+
+    [TestMethod]
+    public async Task UpdateWorkflow()
+    {
+        var (server, logger, office, agentName) = await TestFramework.SetupLoggerOfficeAgentAsync(Storage);
+
+        string state = "", display = "";
+        SemaphoreSlim semaphoreState = new(0, 1), semaphoreDisplay = new(0, 1);
+
+        office.Register(o => o.DataChangedEvent, () =>
+        {
+            office.PostWithResponse<XModel>(
+                agent => agent.GetRequest,
+                model => { state = model.Name + model.Surname; semaphoreState.Release(); });
+        });
+
+        office.Register<string>(o => o.Display, msg => { display = msg; semaphoreDisplay.Release(); });
+
+        office.Post(agent => agent.UpdateRequest, "Marco");
+
+        var sp1 = await semaphoreState.WaitAsync(Timeout);
+        var sp2 = await semaphoreDisplay.WaitAsync(Timeout);
+
+        Assert.IsTrue(sp1);
+        Assert.IsTrue(sp2);
+        Assert.AreEqual("MarcoRossi", state);
+        Assert.AreEqual("Data has been processed", display);
+        Assert.IsTrue(Storage.Any(x => x.Message.Contains(nameof(IHubContract.CreateRequest)) && x.Sender == agentName));
+        Assert.IsTrue(Storage.Any(x => x.Message.Contains(nameof(IContractAgentX.Display)) && x.Sender == agentName));
+        Assert.IsTrue(Storage.Any(x => x.Message.Contains(nameof(IContractAgentX.DataChangedEvent)) && x.Sender == agentName));
+        Assert.IsTrue(Storage.Any(x => x.Message.Contains(nameof(IContractAgentX.UpdateRequest)) && x.Sender == office.Me));
+        Assert.IsTrue(Storage.Any(x => x.Message.Contains(nameof(IAgencyContract.AgentsRegistrationRequest)) && x.Sender == Addresses.Central));
+    }
 }
 
 
